0bbf3dd59ab6488248a277bc94965014badbebed
Antoine Reversat <a.reversat@gmail.com>
2022-12-12 18:10:54 -0500

    Part1 working solution

--- day12/src/bin/part1.rs
+++ day12/src/bin/part1.rs
@@ -4,16 +4,16 @@ use std::process;
 
 use simple_matrix::Matrix;
 
-fn read_input(filename: &str) -> Matrix<char> {
+fn read_input(filename: &str) -> Matrix<(char, usize)> {
     let content = fs::read_to_string(filename).expect("Unable to read from file.");
     let height = content.lines().count();
     let width = content.lines().nth(0).unwrap().trim().chars().count();
-    Matrix::from_iter(height, width, content.chars().filter(|x| *x != '\n' && *x != '\r'))
+    Matrix::from_iter(height, width, content.chars().filter(|x| *x != '\n' && *x != '\r').map(|x| (x, std::usize::MAX)))
 }
 
-fn can_climb(from: (usize, usize), to: (usize, usize), matrix: &Matrix<char>) -> bool {
-    let mut from_char = *matrix.get(from.1, from.0).unwrap();
-    let mut to_char = *matrix.get(to.1, to.0).unwrap(); 
+fn can_climb(from: (usize, usize), to: (usize, usize), matrix: &Matrix<(char, usize)>) -> bool {
+    let mut from_char = matrix.get(from.1, from.0).unwrap().0;
+    let mut to_char = matrix.get(to.1, to.0).unwrap().0; 
 
     if from_char == 'S' {
         from_char = 'a';
@@ -23,11 +23,10 @@ fn can_climb(from: (usize, usize), to: (usize, usize), matrix: &Matrix<char>) ->
         to_char = 'z';
     }
 
-    //u32::from(to_char) <= u32::from(from_char) + 1
-    u32::from(to_char) <= u32::from(from_char) + 1 && u32::from(to_char) >= u32::from(from_char)
+    u32::from(to_char) <= u32::from(from_char) + 1
 }
 
-fn gen_neighbors(from: (usize, usize), matrix: &Matrix<char>) -> Vec<(usize, usize)> {
+fn gen_neighbors(from: (usize, usize), matrix: &Matrix<(char, usize)>) -> Vec<(usize, usize)> {
     let mut ret: Vec<(usize, usize)> = Vec::new();
     if from.0 >= 1 {
         ret.push((from.0 - 1, from.1));
@@ -45,27 +44,15 @@ fn gen_neighbors(from: (usize, usize), matrix: &Matrix<char>) -> Vec<(usize, usi
     ret
 }
 
-fn get_path(from: (usize, usize), prev: Vec<(usize, usize)>, matrix: &Matrix<char>) -> Vec<Vec<(usize, usize)>> {
-    let mut ret: Vec<Vec<(usize, usize)>> = Vec::new();
-    if matrix.get(from.1, from.0).unwrap() == &'E' {
-        ret.push(vec![from]);
-        println!("Found path");
-        return ret;
-    }
-    let mut prev = prev.to_vec();
-    prev.push(from);
+fn get_path(from: (usize, usize), matrix: &mut Matrix<(char, usize)>) {
     for neighbor in gen_neighbors(from, matrix) {
-        if !prev.contains(&neighbor) && can_climb(from, neighbor, matrix) {
-            let mut paths = get_path(neighbor, prev.clone(), matrix);
-            for path in paths.iter_mut() {
-                path.insert(0, from);
-            }
-            if !paths.is_empty() {
-                ret.append(&mut paths);
-            }
+        let n_val = *matrix.get(neighbor.1, neighbor.0).unwrap();
+        let our_val = *matrix.get(from.1, from.0).unwrap();
+        if can_climb(from, neighbor, matrix) && n_val.1 > our_val.1 + 1 {
+            matrix.set(neighbor.1, neighbor.0, (n_val.0, our_val.1 + 1));
+            get_path(neighbor, matrix);
         }
     }
-    ret
 }
 
 fn main() {
@@ -75,20 +62,22 @@ fn main() {
         process::exit(1);
     }
 
-    let contents = read_input(&args[1]);
+    let mut contents = read_input(&args[1]);
     println!("{:?}", contents);
     let mut start = (0,0);
+    let mut end = (0,0);
     for x in 0..contents.cols() {
         for y in 0..contents.rows() {
-            if contents.get(y, x).unwrap() == &'S' {
+            if contents.get(y, x).unwrap().0 == 'S' {
                 start = (x, y);
+                contents.set(y, x, ('S', 0));
+            }
+            if contents.get(y, x).unwrap().0 == 'E' {
+                end = (x, y);
             }
         }
     }
 
-    let mut paths = get_path(start, vec![start], &contents);
-    paths.sort_by_key(|k| k.len());
-    for path in paths {
-        println!("{:?} {}", path, path.len());
-    }
+    get_path(start, &mut contents);
+    println!("{:?}", contents.get(end.1, end.0).unwrap());
 }
